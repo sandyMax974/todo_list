a4a914e3abe020d1922a657bb61a92e2c1c1a7a9
sandy <pounoussamy.sandy@gmail.com>
2021-04-17 14:26:40 +0100

    Initial app setup and first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//middleware
+app.use(cors()); 
+app.use(express.json());
+
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
