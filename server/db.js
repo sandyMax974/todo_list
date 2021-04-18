760c030406275dbcf7e89814d5c37dca3862464d
sandy <pounoussamy.sandy@gmail.com>
2021-04-18 11:29:56 +0100

    Created basic CRUD routes for todos and initialised react client skeleton

--- server/db.js
+++ server/db.js
@@ -1,7 +1,6 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: "admin",
   host: "localhost",
   port: 5432,
   database: "perntodo"
--- server/index.js
+++ server/index.js
@@ -15,16 +15,57 @@ app.post("/todos", async (req, res) => {
   // try/catch to help error handling
   try {
     const { description } = req.body;
+    console.log(description)
     const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
-    [description]
-    );
+    [description]);
     res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 })
 
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1",
+    [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", 
+    [description, id]);
+    res.json("Todo was updated")
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+    res.json("Todo was deleted")
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 //server setup
 const port = 5000;
 app.listen(port, () => {
