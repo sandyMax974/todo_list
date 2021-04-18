9cb1e011b29cf60d30522d295fd84deddac0a72e
sandy <pounoussamy.sandy@gmail.com>
2021-04-18 10:37:36 +0100

    Added route to create new todo

--- server/index.js
+++ server/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors()); 
 app.use(express.json()); 
 
 
+//routes
+
+//async allows to use await
+app.post("/todos", async (req, res) => {
+  // try/catch to help error handling
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
+    [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+
+//server setup
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
